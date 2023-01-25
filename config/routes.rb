54f91b6b5e350ae090faf1b410cb3cebbd29d1cb
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-01-25 12:17:50 +0100

    routing complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+
+  get 'pages/about', to: 'pages#about', as: 'about'
+
+  resources :applications
+  resources :blogposts
+
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
