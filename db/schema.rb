6fdd046311883b11a3f620cb93a48512aa8b2d3a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-02 16:41:43 +0100

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_143113) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_150240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_143113) do
   create_table "blogposts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "content"
   end
 
   create_table "candidates", force: :cascade do |t|
