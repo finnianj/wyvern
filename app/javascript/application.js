d2b911380fea7f92aa17d1989a528ed00159db05
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-08 17:43:43 +0100

    finally working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,10 +5,4 @@ import "trix"
 import "@rails/actiontext"
 import Typed from 'typed.js';
 
-var homepage = new Typed('#homepage-title', {
-  strings: ["Wyvern Art Fund ^1000", "Supporting creativity^1000"],
-  typeSpeed: 50,
-  showCursor: false,
-  loop: true,
-  fadeOut: true
-});
+
