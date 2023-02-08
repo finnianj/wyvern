ef2926e8fca87468a333c6872b0a9b06739bd02f
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-08 16:35:18 +0100

    ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "trix"
 import "@rails/actiontext"
+import Typed from 'typed.js';
+
+var homepage = new Typed('#homepage-title', {
+  strings: ["Wyvern Art Fund ^1000", "Supporting creativity^1000"],
+  typeSpeed: 50,
+  showCursor: false,
+  loop: true
+});
+
