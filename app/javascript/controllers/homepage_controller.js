2bfb5b903149e1d057bfb735374c509012634c15
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-12 16:32:31 +0100

    typed js solved, stimulus added

---
+++ app/javascript/controllers/homepage_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from 'typed.js';
+
+// Connects to data-controller="homepage"
+export default class extends Controller {
+  connect() {
+    console.log("controller connected")
+    var homepage_title = new Typed('#homepage-title', {
+        strings: ["Wyvern Art Fund ^1000", "Supporting creativity^1000"],
+        typeSpeed: 80,
+        showCursor: false,
+        loop: true,
+        fadeOut: true
+      });
+  }
+
+
+
+}
